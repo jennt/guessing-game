af57333887a142354840937b71baaaa71ff0b25d
Jenn Tustin <jenn.tustin@gmail.com>
2017-01-11 15:04:54 -0500

    Exits after 5 guesses

--- game.rb
+++ game.rb
@@ -9,10 +9,17 @@ def input_valid?(guess, range)
   end
 end
 
-def check_guess(guess, answer)
-  puts 'Too high' if answer < guess
-  puts 'Too low'  if answer > guess
-  puts 'You won'  if answer == guess
+def check_guess(guess, answer, tries)
+  if tries == 5
+    puts "you lose"
+  elsif answer < guess
+    puts 'Too high'
+  elsif answer > guess
+    puts 'Too low'
+  else answer == guess
+    puts 'You won'
+
+  end
 end
 
 tries = 0
@@ -22,12 +29,19 @@ answer = rand(100)
 puts "guess a number"
 guess = gets.chomp.to_i
 
-until tries == 4
+until tries == 5
 
   input_valid?(guess, range)
-  check_guess(guess, answer)
+  check_guess(guess, answer, tries)
   tries += 1
+
+  if tries == 5
+    puts "5 tries you lose"
+    exit
+  end
+
   puts "Guess again"
   guess = gets.chomp.to_i
-
+  puts "Answer: #{answer}"
+# binding.pry
 end
