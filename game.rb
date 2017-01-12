a7e29aa6dd92647c9600e507fdca5a808aec3998
Jenn Tustin <jenn.tustin@gmail.com>
2017-01-11 23:38:34 -0500

    Add numeric validation

--- game.rb
+++ game.rb
@@ -1,24 +1,39 @@
 require 'pry'
-#test if number, rescue?? same guess twice
+
+def is_numeric?(guess, tries)
+  begin
+    return Integer(guess)
+  rescue
+    puts "Not a valid guess. Guess again"
+    guess = gets.chomp
+    tries += 1
+    if tries == 5
+      puts "you lose. It took #{tries} tries."
+      exit
+    end
+    is_numeric?(guess, tries)
+  end
+end
 
 def input_valid?(guess, range)
   begin
-  range.include?(guess)
+    guess = guess.to_i
+    range.include?(guess)
   rescue
-  puts "Not a valid guess. Guess again"
-  guess = gets.chomp
+    puts "Guess not in range. Guess again"
+    guess = gets.chomp
   end
 end
 
 def check_guess(guess, answer, tries)
   if tries == 5
-    puts "you lose"
-  elsif answer < guess
+    puts "you lose. It took #{tries} tries."
+  elsif answer < guess.to_i
     puts 'Too high'
-  elsif answer > guess
+  elsif answer > guess.to_i
     puts 'Too low'
-  else answer == guess
-    puts 'You won!'
+  else answer == guess.to_i
+    puts "You won! It took #{tries} tries."
     exit
   end
 end
@@ -28,15 +43,17 @@ range = [(1..100)]
 answer = rand(100)
 
 puts "guess a number"
-guess = gets.chomp.to_i
+guess = gets.chomp
+tries += 1
 
-until tries == 5
+until tries == 7
 
+  is_numeric?(guess, tries)
   input_valid?(guess, range)
   check_guess(guess, answer, tries)
   tries += 1
 
-  if tries == 5
+  if tries == 6
     puts "5 tries you lose"
     exit
   end
@@ -45,3 +62,6 @@ until tries == 5
   guess = gets.chomp.to_i
   puts "Answer: #{answer}"
 end
+
+#same guess twice
+# take 6 tries when you mix numbers n letters(letters first)
